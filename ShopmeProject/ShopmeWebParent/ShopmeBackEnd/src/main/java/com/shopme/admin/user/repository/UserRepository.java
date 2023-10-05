c371204b80fc71fa810242beafbebfa42c4e04c3
221hussein <yuzinpream51@gmail.com>
2023-10-05 18:34:37 +0000

    coding user authentication

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/repository/UserRepository.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/repository/UserRepository.java
@@ -8,13 +8,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends PagingAndSortingRepository<User,Integer> {
 
     @Query("SELECT u from User u WHERE u.email = :email")
-    public User getUserByEmail(@Param("email")String email);
+    public User getUserByEmail(@Param("email") String email);
 
     public Long countById(Integer id);
 
